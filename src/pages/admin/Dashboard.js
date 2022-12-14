41eca6a8e3a63f22e4978e584d540d64ad9a7d08
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-12-14 13:59:57 +0100

    span added

--- src/pages/admin/Dashboard.js
+++ src/pages/admin/Dashboard.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
 
 export default function Dashboard() {
-  const count = useState;
+    const count = useState;
 
-  return (
-    <section className="page">
-      <h1>Admin Dashboard</h1>
-      <article>
-        <p>Bought Items: {count}</p>
-      </article>
-    </section>
-  );
+    return (
+        <section className="page">
+            <h1>Admin Dashboard</h1>
+            <article>
+                <p>Bought Items: {count}</p>
+                <span></span>
+            </article>
+        </section>
+    );
 }
